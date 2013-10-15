a6766d8deed52204ba98b4364770bb08e31d9111
Martin Isenburg <martin@rapidlasso.com>
2013-10-15 12:11:03 +0200

    added validation for wave packet descriptor

--- src/lasvalidate.cpp
+++ src/lasvalidate.cpp
@@ -39,7 +39,7 @@
 #include "xmlwriter.hpp"
 #include "lascheck.hpp"
 
-#define VALIDATE_VERSION  131007
+#define VALIDATE_VERSION  131015
 
 #define VALIDATE_PASS     0x0000
 #define VALIDATE_FAIL     0x0001
